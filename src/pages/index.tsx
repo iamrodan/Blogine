6ba0fa7dc5bf8818dd83f0a59ec203ea6e6f4a18
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-07-21 18:21:20 +0545

    fix: redirection from home page to loginpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
-  const { push } = useRouter();
+  const router = useRouter();
   const isLoggedIn = false;
-
-  if (!isLoggedIn) {
-    push("/login");
-    return;
-  }
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.replace("/login");
+    }
+  }, [isLoggedIn, router]);
 
   return <main>Home Page</main>;
 }
